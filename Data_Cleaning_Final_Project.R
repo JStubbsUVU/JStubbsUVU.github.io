58c5be0f88add167ee7f38c98e75fbf0b22c43d1
Your Name <you@example.com>
2023-12-13 23:49:46 -0700

    website

--- Data_Cleaning_Final_Project.R
+++ Data_Cleaning_Final_Project.R
@@ -4,14 +4,21 @@ library(tidyverse)
 library(readxl)
 library(readr)
 library(ggplot2)
+library(kableExtra)
+library(broom)
+library(webshot)
+library(knitr)
+library(ggplot2)
+library(gganimate)
+webshot::install_phantomjs()
 
 
-EV_1 <- read_excel("EV battery pack capacity per vehicle in the US market 2022.xlsx")
-View(EV_1)
+#EV_1 <- read_excel("EV battery pack capacity per vehicle in the US market 2022.xlsx")
+#View(EV_1)
 
 
-EV_2 <- read_csv("EV Range _Value_ - General.csv")
-View(EV_2)
+#EV_2 <- read_csv("EV Range _Value_ - General.csv")
+#View(EV_2)
 
 EV_3 <- read_csv("EV Release Database - EV List.csv")
 
@@ -137,4 +144,68 @@ model <- lm(`EVR (mi)` ~ `Batt kWh` + `Release Date`, data = EV_3)
 # Print the model summary
 summary(model)
 
-broom tidy 
+
+# Create a scatter plot with the regression line
+plot <- ggplot(EV_3, aes(x = `Batt kWh`, y = `EVR (mi)`)) +
+  geom_point() +
+  geom_smooth(method='lm') +
+  labs(title = "Linear Regression Plot",
+       x = "Batt kWh",
+       y = "EVR(mi)")
+
+# Save the plot as an image file (e.g., PNG)
+ggsave("linear_regression_plot.png", plot, width = 8, height = 6)
+
+
+
+
+
+
+
+
+
+
+table <- broom::tidy(model)
+
+# Use knitr to create an HTML table
+html_table <- knitr::kable(table, format = "html", escape = FALSE) %>%
+  kable_styling(full_width = FALSE, position = "center", bootstrap_options = "striped") %>%
+  row_spec(0, background = "white", color = "black")  # Set header background to white
+
+# Modify the HTML style to set the background color to white
+html_table <- gsub('<table', '<table style="background-color: white;"', html_table)
+
+# Save the modified HTML table as an HTML file
+writeLines(as.character(html_table), "linear_regression_summary.html")
+
+# Use webshot to capture the modified HTML table and save as a PNG file
+webshot("linear_regression_summary.html", "linear_regression_summary.png")
+
+#animation
+
+# Load your image
+ev_image <- readPNG("Media/Mach E.png")
+
+ev_image
+
+# Create a static plot with ggplot
+static_plot <- ggplot() +
+  annotation_custom(rasterGrob(ev_image, width = 1, height = 1), xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
+  theme_void()
+
+# Create an animated plot using gganimate
+animated_plot <- static_plot +
+  transition_states(states = 1:10, transition_length = 1, state_length = 1) +
+  enter_fade() +
+  exit_fade()
+
+# Save the animation as a GIF
+anim_save("ev_animation.gif", animate(animated_plot))
+
+
+
+
+
+
+
+
